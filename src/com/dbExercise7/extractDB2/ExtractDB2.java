52d7d0fd397d96ebac736cb333b30c3960d478a2
Khooshal Saurty <asaurty@yahoo.co.uk>
2016-07-04 17:05:41 +0200

    Data Manipulation - still in progress

--- src/com/dbExercise7/extractDB2/ExtractDB2.java
+++ src/com/dbExercise7/extractDB2/ExtractDB2.java
@@ -4,7 +4,9 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
+import com.dbExercise7.data.ProductCategoryId;
 import com.dbExercise7.util.DB2ConnectionManager;
 
 
@@ -12,14 +14,15 @@ public class ExtractDB2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		list();
-	}
-	
-	public static void extractFromDB(){
 		
 	}
 	
-	public static String getInsertsArticleId(){
+	public static void extractFromDB(){
+		getTableProductCategoryId();
+	}
+	
+	public static ArrayList<ProductCategoryId> getTableProductCategoryId(){
+		ArrayList<ProductCategoryId> objects = new ArrayList<>();
 		try {
 			// Get connection
 			Connection con = DB2ConnectionManager.getInstance().getConnection();
@@ -30,26 +33,92 @@ public class ExtractDB2 {
 
 			// Processing result
 			ResultSet rs = pstmt.executeQuery();
-			String outSQL = "INSERT INTO VSISP51.PRODUCTCATEGORYID (PRODUCTCATEGORYID, NAME) VALUES ";
-			
 			while (rs.next()) {
-				String str = "";
-				str += rs.getInt("PRODUCTCATEGORYID");
-				str += rs.getString("NAME");
-				str += ", ";
-				str += rs.getDouble("preis");
-				str += ".";
-				
-				System.out.println(str);
+				objects.add(new ProductCategoryId(rs.getInt("PRODUCTCATEGORYID"), rs.getString("NAME")));
 			}
 			rs.close();
 			pstmt.close();
 			DB2ConnectionManager.getInstance().closeConnection();
-			return str;
+			
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
-		return "";
+		return objects;
+	}
+	
+	public static ArrayList<ProductFamilyId> getTableProductFamilyId(){
+		ArrayList<ProductFamilyId> objects = new ArrayList<>();
+		try {
+			// Get connection
+			Connection con = DB2ConnectionManager.getInstance().getConnection();
+			
+			// Prepare Statement
+			String selectSQL = "SELECT * FROM DB2INST1.PRODUCTFAMILYID";
+			PreparedStatement pstmt = con.prepareStatement(selectSQL);
+
+			// Processing result
+			ResultSet rs = pstmt.executeQuery();
+			while (rs.next()) {
+				objects.add(new ProductFamilyId(rs.getInt("PRODUCTFAMILYID"), rs.getInt("PRODUCTCATEGORYID"), rs.getString("NAME")));
+			}
+			rs.close();
+			pstmt.close();
+			DB2ConnectionManager.getInstance().closeConnection();
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return objects;
+	}
+	
+	public static ArrayList<ProductGroupId> getTableProductGroupId(){
+		ArrayList<ProductGroupId> objects = new ArrayList<>();
+		try {
+			// Get connection
+			Connection con = DB2ConnectionManager.getInstance().getConnection();
+			
+			// Prepare Statement
+			String selectSQL = "SELECT * FROM DB2INST1.PRODUCTGROUPID";
+			PreparedStatement pstmt = con.prepareStatement(selectSQL);
+
+			// Processing result
+			ResultSet rs = pstmt.executeQuery();
+			while (rs.next()) {
+				objects.add(new ProductGroupId(rs.getInt("PRODUCTGROUPID"), rs.getInt("PRODUCTFAMILYID"), rs.getString("NAME")));
+			}
+			rs.close();
+			pstmt.close();
+			DB2ConnectionManager.getInstance().closeConnection();
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return objects;
+	}
+	
+	public static ArrayList<ArticleId> getTableArticleId(){
+		ArrayList<ArticleId> objects = new ArrayList<>();
+		try {
+			// Get connection
+			Connection con = DB2ConnectionManager.getInstance().getConnection();
+			
+			// Prepare Statement
+			String selectSQL = "SELECT * FROM DB2INST1.ARTICLEID";
+			PreparedStatement pstmt = con.prepareStatement(selectSQL);
+
+			// Processing result
+			ResultSet rs = pstmt.executeQuery();
+			while (rs.next()) {
+				objects.add(new ArticleId(rs.getInt("ARTICLEID"), rs.getInt("PRODUCTGROUPID"), rs.getString("NAME"), rs.getDouble("PREIS")));
+			}
+			rs.close();
+			pstmt.close();
+			DB2ConnectionManager.getInstance().closeConnection();
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return objects;
 	}
 	
 	public boolean save() {
