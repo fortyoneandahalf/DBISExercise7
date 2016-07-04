95b74abbcd5aeb474595d0c8da0891346b72f904
fortyoneandahalf <phil_bradfield@hotmail.com>
2016-07-04 15:15:52 +0200

    Initial commit

---
+++ src/com/dbExercise7/etl/ETLMain.java
@@ -0,0 +1,15 @@
+package com.dbExercise7.etl;
+
+public class ETLMain
+{
+
+	public static void main(String[] args)
+	{
+		// TODO Extract dimensions from DB2
+		
+		// TODO Extract sales data from sales.csv
+		
+		// TODO Load extracted data into data warehouse
+	}
+
+}
---
+++ src/com/dbExercise7/viewer/CubeViewer.java
@@ -0,0 +1,11 @@
+package com.dbExercise7.viewer;
+
+public class CubeViewer
+{
+
+	public static void main(String[] args)
+	{
+		// TODO
+	}
+
+}
